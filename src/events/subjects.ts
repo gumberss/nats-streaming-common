a1e2f92622d35c95d69ce4dc5b4cc2faa9650935
Gumbers <gustavomarinsuppi@gmail.com>
2020-06-07 15:17:07 -0300

    feat: adding base listener and base publisher as abstract classes that must be inherited by concrete classes. Subjects that must contains all subjets existent in your project, ticket created and updated event that must be the events shared in your microservices

---
+++ src/events/subjects.ts
@@ -0,0 +1,9 @@
+/**
+ * Here you must define all the subjects that 
+ * exists in your microsservice world as you
+ *  can see the examples below
+ */
+export enum Subjects {
+  TicketCreated = 'ticket:created',
+  TicketUpdated = 'ticket:updated',
+}
---
+++ src/events/ticket-updated-event.ts
@@ -0,0 +1,18 @@
+/**
+ * In this file, you must define the interfaces
+ * of your event (one for each event)
+ */
+
+import { Subjects } from './subjects'
+
+export interface TicketUpdatedEvent {
+	subject: Subjects.TicketUpdated
+	data: TicketUpdateEventData
+}
+
+export interface TicketUpdateEventData {
+	id: string
+	title: string
+  price: number
+  userId: string
+}
