f5a2be3b8dcd330d072e760c157290bf68564487
Gumbers <gustavomarinsuppi@gmail.com>
2020-06-11 18:56:04 -0300

    feat: adding order status

---
+++ src/events/types/order-status.ts
@@ -0,0 +1,21 @@
+export enum OrderStatus {
+  /**
+   * When the order has been created, but the ticket
+   * it is triyng to order has not been reserved
+   */
+  Created = 'created',
+  /**
+   * The ticket the order is trying to reserve has already
+   * been reserverd, or when the user has cancelled the order
+   */
+  Cancelled = 'cancelled',
+  /**
+   * The order has successfully reserved the ticket
+   */
+  AwaitingPatment = 'await:payment',
+  /**
+   * The order has reserved the ticket and the 
+   * user has provided payment successfully
+   */
+  Complete = 'complete'
+}
